487d928589eae57eb4af605adae0cd83489fbae5
yuhongtao <yuhongtaoaaa@163.com>
2021-04-13 00:32:20 +0800

    connect neo4j

--- src/main/java/com/seu/CMT/controller/ModifyController.java
+++ src/main/java/com/seu/CMT/controller/ModifyController.java
@@ -1,23 +1,35 @@
 package com.seu.CMT.controller;
 
+import com.seu.CMT.mapper.ModifyMapper;
+import com.seu.CMT.model.UserModel;
 import com.seu.CMT.pojo.Film;
 import com.seu.CMT.pojo.ExcelData;
 import com.seu.CMT.pojo.ResultDTO;
 import com.seu.CMT.service.ModifyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@Controller
+import javax.management.openmbean.CompositeDataSupport;
+import java.util.List;
+
+@RestController
 @RequestMapping({"modify"})
 
 public class ModifyController {
     @Autowired
     ModifyService modifyService;
+    @Autowired
+    private ModifyMapper modifyMapper;
+
     public ModifyController(){
+    }
+
+    @GetMapping("/getAll")
+    public List<UserModel> getAll() {
+        List<UserModel> models = modifyMapper.getAll();
+        return models;
     }
 
     @RequestMapping("/addExcel")
